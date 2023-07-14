90f6cb7a6b2c8360bec06b850d9bf96f36448c5b
Thomas Szentirmay <tlszent@outlook.com>
2023-07-13 21:16:50 -0400

    added controllers for blog, user and views

---
+++ controllers/user_routes.js
@@ -0,0 +1,60 @@
+const router = require('express').Router();
+const User = require('../models/User')
+
+// Login User
+router.post('/login', async (req, res) => {
+    try {
+        const userName = req.body.userName;
+        const formPassword = req.body.password;
+
+        const user = await User.findOne({
+            where: {
+                userName: userName
+            }
+        });
+
+        // if the user doesnt exist, redirect them to register
+        if(!user) return res.redirect('/register');
+
+        // Validate that the password is a match
+        const isValidPass = await user.validatePass(formPassword);
+
+        if (!isValidPass) throw new Error('invalid_password');
+
+        // user has been validated, and now we log them in by creating a session
+        req.session.user_id = user.id;
+
+        res.redirect('/dashboard');
+
+    } catch (err) {
+        if (err.message === 'invalid_password') {
+            res.redirect('/login'); 
+        }
+    }
+});
+
+// Register User
+router.post('/register', async (req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+
+        // Creates a session and sends a cookie to the client
+        req.session.user_id = newUser.id;
+
+        res.redirect('/dashboard');
+    } catch (err) {
+        const dupeName = err.errors.find(e => e.path === 'userName');
+
+        // if email already exists, redirect them to the login page
+        if (dupeName) res.redirect('/login');
+    }
+})
+
+// Log out User
+router.get('/logout', (req, res) => {
+    req.session.destroy();
+
+    res.redirect('/');
+})
+
+module.exports = router;
---
+++ controllers/view_routes.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const Blog = require('../models/Blog')
+
+function isAuthenticated(req, res, next) {
+  const isAuthenticated = req.session.user_id;
+
+  if (!isAuthenticated) return res.redirect('/login');
+
+  next();
+}
+
+// Show Homepage
+router.get('/', async (req, res) => {
+  let blogs = await Blog.findAll({
+    include: User
+  });
+
+  blogs = blogs.map(t => t.get({plain: true}))
+
+  res.render('index', {
+    isHome: true,
+    isLoggedIn: req.session.user_id,
+    blogs: blogs
+  });
+});
+
+// Show Login Page
+router.get('/login', (req, res) => {
+  if (req.session.user_id) return res.redirect('/dashboard')
+
+  return res.render('login', {
+    isLogin: true
+  });
+});
+
+// Show Register Page
+router.get('/register', (req, res) => {
+  if (req.session.user_id) return res.redirect('/dashboard')
+
+  res.render('register', {
+    isRegister: true
+  });
+});
+
+// Show Dashboard Page
+router.get('/dashboard', isAuthenticated, async (req, res) => {
+  const user = await User.findByPk(req.session.user_id, {
+    include: Blog
+  });
+
+  const blogs = user.blogs.map(t => t.get({plain: true}))
+
+  // The user IS logged in
+  res.render('dashboard', {
+    userName: userName,
+    blogs: blogs
+  });
+});
+
+module.exports = router;
